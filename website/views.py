7af0bc7ceddc3f8ed2a93fae12d4b7f49a905591
EwaDwornik <ewaannadwornik@gmail.com>
2021-04-02 18:27:18 +0000

    still working on calculators

--- website/views.py
+++ website/views.py
@@ -28,122 +28,70 @@ def thanks(view):
 class ProgrammingView(TemplateView):
     pass
 
+
 class KnittingView(TemplateView):
     template_name = 'knitting.html'
 
+
 class HerbsView(TemplateView):
     pass
+
 
 class MusicView(TemplateView):
     template_name = 'music.html'
 
 
 class CalculatorsView(TemplateView):
-    template_name = "calculators.html"
+    template_name = 'calculators.html'
 
     def get(self, request, *args, **kwargs):
-        form2 = PrimesForm()
-        form3 = Primes2Form()
-        context = {'form2': form2,
-                   'form3': form3}
-        return render(request, 'calculators.html', context)
-
-    def post(self, request, *args, **kwargs):
-        if request.method == "POST":
-            if "prime" in request.POST:
-                form2 = PrimesForm(data=request.POST, prefix="banned")
-                if form2.is_valid():
-                    number = form2.cleaned_data['number']
-                    if number > 1:
-                        for i in range(2, number):
-                            if (number % i) == 0:
-                                return render(request,
-                                              'isnotprime.html',
-                                              {"number": number, 'form2': form2},
-                                              )
-                        else:
-                            return render(request,
-                                          'isprime.html',
-                                          {"number": number, 'form2': form2},
-                                          )
-                    else:
-                        return render(request,
-                                      'isnotprime.html',
-                                      {"number": number, 'form2': form2},
-                                      )
-                form3 = Primes2Form(prefix='expected')
-
-
-            elif "prime2" in request.POST:
-                form3 = Primes2Form(request.POST, prefix='expected')
-                if form3.is_valid():
-                    number2 = form3.cleaned_data['number2']
-                    number3 = form3.cleaned_data['number3']
-                    lst = []
-                    for num in range(number2, number3 + 1):
-                        if num > 1:
-                            for i in range(2, num):
-                                if (num % i) == 0:
-                                    break
-                            else:
-                                lst.append(num)
-
-                    return render(request,
-                                  'primes2.html',
-                                  {"number2": number2,
-                                   "number3": number3,
-                                   "lst": lst,
-                                   'form3': form3},
-                                  )
-
-                form2 = PrimesForm(prefix='banned')
-
-        else:
-            form2 = PrimesForm(prefix='banned')
-            form3 = Primes2Form(prefix='expected')
-            context = {'form2': form2,
-                       'form3': form3}
-            return render(request, 'calculators.html', context)
+        form2 = PrimesForm(self.request.GET or None)
+        form3 = Primes2Form(self.request.GET or None)
+        context = self.get_context_data(**kwargs)
+        context['form2'] = form2
+        context['form3'] = form3
+        return self.render_to_response(context)
 
 
 class PrimesView(View):
     """
-    Checking if a number is a prime number.
+    Checking if a number is a prime number and showing all prime numbers between two numbers.
     """
 
-    def get(self, request, *args, **kwargs):
-        form2 = PrimesForm()
-        context = {'form2': form2}
-        return render(request, 'primes.html', context)
-
     def post(self, request, *args, **kwargs):
-        form2 = PrimesForm(data=request.POST)
+        form2 = self.form_class(request.POST)
+        form3 = Primes2Form()
         if form2.is_valid():
             number = form2.cleaned_data['number']
             if number > 1:
                 for i in range(2, number):
                     if (number % i) == 0:
-                        return render(request,
-                                      'isnotprime.html',
-                                      {"number": number, 'form2': form2},
-                                      )
+                        number = "It is not a prime"
+                        return self.render_to_response(
+                            self.get_context_data(number=number, form2=form2, form3=form3
+                                                  )
+                        )
 
                 else:
-                    return render(request,
-                                  'isprime.html',
-                                  {"number": number, 'form2': form2},
-                                  )
+                    number = "It is a prime"
+                    return self.render_to_response(
+                        self.get_context_data(number=number, form2=form2, form3=form3
+                                              )
+                    )
             else:
-                return render(request,
-                              'isnotprime.html',
-                              {"number": number, 'form2': form2},
-                              )
+                number = "It is not a prime"
+                return self.render_to_response(
+                    self.get_context_data(number=number, form2=form2, form3=form3
+                                          )
+                )
 
         else:
-            return render(request,
-                          'primes.html',
-                          {'form2': form2},
-                          )
+
+            return self.render_to_response(
+
+                self.get_context_data(
+                    form2=form2, form3=form3
+                ))
 
 
 class Primes2View(View):
@@ -151,13 +99,9 @@ class Primes2View(View):
     Showing all prime numbers between two numbers.
     """
 
-    def get(self, request, *args, **kwargs):
-        form3 = Primes2Form()
-        context = {'form3': form3}
-        return render(request, 'primes2.html', context)
-
     def post(self, request, *args, **kwargs):
-        form3 = Primes2Form(data=request.POST)
+        form3 = self.form_class(request.POST)
+        form2 = PrimesForm()
         if form3.is_valid():
             number2 = form3.cleaned_data['number2']
             number3 = form3.cleaned_data['number3']
@@ -170,17 +114,17 @@ class Primes2View(View):
                     else:
                         lst.append(num)
 
-            return render(request,
-                          'primes2.html',
-                          {"number2": number2,
-                           "number3": number3,
-                           "lst": lst,
-                           'form3': form3},
-                          )
+            return self.render_to_response(
+                self.get_context_data(number2=number2, number3=number3, lst=lst, form2=form2, form3=form3
+                                      )
+            )
+
         else:
-            return render(request,
-                          'primes2.html',
-                          {'form3': form3}, )
+            return self.render_to_response(
+
+                self.get_context_data(
+                    form2=form2, form3=form3
+                ))
 
 
 class DivisorsView(View):
